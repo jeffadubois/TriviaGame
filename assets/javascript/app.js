fc147bf7853462670493e58af3616d8c220d128c
Jeff DuBois <jeff.adubois@gmail.com>
2017-12-20 04:27:40 -0600

    initial coding of TriviaGame

---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+var theTimeOut;
+var maxSecondsForGame = 25;
+var maxMilliseconds = 25000;
+var secondsRemaining = 25;
+var theCountDown;
+var numberCorrect = 0;
+var numberInCorrect = 0;
+
+var gameOn = false;
+
+$(document).ready(function(){
+
+//clicking on the start button starts the game.
+$('#startbtn').on('click', function (e) {
+ 	console.log("clicked on startbtn");
+	
+    alert( "Note - You have " + maxSecondsForGame +  " seconds to answer the trivia questions." );
+    gameOn = true;
+	theCountDownInterval = setInterval(countDown, 1000);
+	theTimeOut = setTimeout(tallyResults, maxMilliseconds);  
+	$("this").prop("disabled", true);
+	$("#startbtn").prop("disabled", true);
+	secondsRemaining = maxSecondsForGame;
+	numberCorrect = 0;
+	numberInCorrect = 0;
+    $("#abody").html("<h2>You have " + secondsRemaining + " seconds left</h2>");
+
+});
+
+
+function tallyResults(){
+	gameOn = false;
+	numberCorrect = 0;
+	var totalNum = 2;
+    var q1Win =  $("input#radio121").prop("checked");
+     var q2Win =  $("input#radio131").prop("checked");
+	 if (q1Win == true){
+		   numberCorrect++;
+	 }
+	 if (q2Win == true){
+		   numberCorrect++;
+	 }
+
+     numberInCorrect = totalNum - numberCorrect;
+
+	 clearTimeout(theTimeOut);
+	 clearInterval(theCountDownInterval);
+	 
+	 $("#abody").html("<h2>You had " + numberCorrect +  " correct, and " + numberInCorrect + " incorrect!</h2>");
+	 $("#startbtn").prop("disabled", false);
+
+}
+
+
+function countDown() {
+	if (gameOn == true){
+	     secondsRemaining--;
+	     $("#abody").html("<h2>You have " + secondsRemaining + " seconds left</h2>");
+	     if (secondsRemaining <= 0){
+			 gameOn = false;
+	     }
+	}
+}
+
+});// end document ready
+
+
